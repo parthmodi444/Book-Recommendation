0fc196016a2d5715e2cd758ca19dc6956f2cef6c
parthmodi444 <88581955+parthmodi444@users.noreply.github.com>
2021-09-26 02:20:12 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -5,30 +5,36 @@ const favBook = {
   ComputerSci: [
     {
       Name: "The algorithms",
+      Desc: "It consist of various algo in c",
       Rating: "⭐⭐⭐⭐⭐"
     },
     {
       Name: "The Code",
+      Desc: "It contains Python code ",
       Rating: `⭐⭐⭐⭐⭐`
     },
     {
       Name: "Clean Code",
+      Desc: "It describes abt how to write efficient code",
       Rating: `⭐⭐⭐⭐⭐`
     }
   ],
   Business: [
     {
       Name: "Think an grow rich ",
+      Desc: "It focuses on mind-power",
       Rating: `⭐⭐⭐⭐⭐`
     },
 
     {
       Name: "Zero to one",
+      Desc: "It focuses that with hardwork we can achieve anything",
       Rating: `⭐⭐⭐⭐`
     },
 
     {
       Name: "Four hour work week",
+      Desc: "It says tha consistency is the key",
       Rating: `⭐⭐⭐⭐`
     }
   ],
@@ -36,14 +42,17 @@ const favBook = {
   Pyschology: [
     {
       Name: "Thinking fast and slow",
+      Desc: "It describes the state of mind",
       Rating: `⭐⭐⭐⭐`
     },
     {
       Name: "Influence",
+      Desc: "It describes abt the influencer in lyf",
       Rating: `⭐⭐⭐⭐⭐`
     },
     {
       Name: "The social animal",
+      Desc: "We Should be socially active",
       Rating: `⭐⭐⭐⭐⭐`
     }
   ]
@@ -58,11 +67,13 @@ export default function App() {
   function getBookpost(item) {
     var nama = item.Name;
     var rata = item.Rating;
+    var desc = item.Desc;
     var bookList = (
       <li>
         <div style={{ padding: "0rem 1rem" }}>
           <h3>{nama}</h3>
           <p style={{ fontSize: "0.8rem" }}>{rata}</p>
+          <h5>{desc}</h5>
         </div>
       </li>
     );
